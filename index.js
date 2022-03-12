057f0ee217548711deaae716d0cf8f569728f5d0
asatyrjan <50033356+asatyrjan@users.noreply.github.com>
2022-03-12 15:55:41 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+Function: drip(address _recipient)
+
+MethodID: 0x67a5cd06
+[0]:  00000000000000000000000030ee10ad786125b5a433b7881ba826e8411720ce
+Function: drip(address _recipient)
+
+MethodID: 0x67a5cd06
+[0]:  00000000000000000000000030ee10ad786125b5a433b7881ba826e8411720ce
+Function: drip(address _recipient)
+
+MethodID: 0x67a5cd06
+[0]:  00000000000000000000000030ee10ad786125b5a433b7881ba826e8411720ce
